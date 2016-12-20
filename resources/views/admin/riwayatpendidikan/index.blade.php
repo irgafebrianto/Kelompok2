37e1af53642fb00ff692d27a800b71dc0b84a04e
acer\lerry <immanuelvalerie1@gmail.com>
2016-12-19 23:16:44 -0800

    penemuan terbaru

--- resources/views/admin/riwayatpendidikan/index.blade.php
+++ resources/views/admin/riwayatpendidikan/index.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="panel panel-success">
                 <div class="panel-heading">
-                    <h2 class="panel-title" align="center"> Data Anak Pegawai</h2>
+                    <h2 class="panel-title" align="center"> Data Riwayat Pendidikan</h2>
                 </div>
                     <div class="panel-body"> 
                         
